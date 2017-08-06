bad0e596e0f8d808abdb4711294396d93edf7685
Elvin Granat <elvgr805@student.liu.se>
2017-08-06 16:43:52 +0200

    Made the simplest website ever

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  // templateUrl: './app.profileComponent.html',
+  template: `
+  <h1> Hello, {{name}}</h1>
+  <h2><i>Address:</i> {{address["city"]}}, {{address.city}}</h2>
+  <h2><i>Email:</i>{{email}}</h2>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app';
+  name = 'Elvin';
+  email = 'elvin@gmail.com';
+  address = {
+    street : 'Rydsvägen 1',
+    city : 'Linköping',
+    county : 'Östergötland'
+  };
 }
